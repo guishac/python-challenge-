2d818dbd3ad67da94d5bbc4d993f5f3a1d2d0e2c
guishac <52980433+guishac@users.noreply.github.com>
2020-01-18 21:59:07 -0500

    Add files via upload

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,10 +7,11 @@ budget_csv = ("budget_data.csv")
 months_total = 0
 total_net = 0
 average_change = 0
+prevmonth = 0
 net_list = []
 months_list = []
 greatest_increase = ["", 0]
-greatest_decrease = ["", 9999999]
+greatest_decrease = ["", 9999999999999999999]
 # with open("Resources/budget_data.csv", newline="") as csvfile:
 with open(csvpath, newline="") as csvfile:
 
@@ -20,39 +21,47 @@ with open(csvpath, newline="") as csvfile:
     first_row = next(csvreader)
 
 # The total number of months included in the dataset
-    # months_total = months_total + 1
-    # total_net = total_net + int(first_row[1])
-    # prev_net = int(first_row[1])
+    months_total = months_total + 1
+    total_net = total_net + int(first_row[1])
+    prev_net = int(first_row[1])
 
     for row in csvreader:
          months_total = months_total + 1
         #  total_net = total_net + int(first_row[1])
-         prev_net = int(first_row[1])
-        #  month_total = +1
+        #  prev_net = int(first_row[1])
+         
          total_net = total_net + int(row[1])
-    # net change
-    net_change = int(row[1]) - prev_net
-    prev_net = int(row[1])
-    net_list = net_list + [net_change]
-    months_list = months_list + [row[0]]
-    # greatest increase
-    if net_change > greatest_increase[1]:
-        greatest_increase[1] = net_change
-        greatest_increase[0] = row[0]
 
-    if net_change < greatest_decrease[1]:
-        greatest_decrease[0] = row[0]
-        greatest_decrease[1] = net_change
+         net_change = int(row[1]) - prev_net
+         prev_net = int(row[1])
+         net_list = net_list + [net_change]
+         months_list = months_list + [row[0]]
+
+
+    
+    # greatest increase
+         if (net_change > greatest_increase[1]):
+            greatest_increase[1] = net_change
+            greatest_increase[0] = row[0]
+
+         if (net_change < greatest_decrease[1]):
+           greatest_decrease[0] = row[0]
+           greatest_decrease[1] = net_change
+        
+
+    
 average_netchange = sum(net_list)/len(net_list)
+
+
 output = (
     f"\nFinancial Analysis\n"
     f"----------------------------\n"
     f"Total Months: {months_total}\n"
     f"Total: ${total_net}\n"
-    f"Average  Change: ${average_netchange:.2f}\n"
+    f"Average  Change: ${average_netchange}\n"
     f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})\n"
     f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n")
-
+# :.2f
 
 print(output)
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -20,11 +20,12 @@ with open("election_data.csv", "r") as csvfile:
     for row in csvreader:
         total_votes = total_votes + 1
         candidate = row[2]
-        if candidate in candidate_votes:
-            candidate_votes[candidate] = candidate_votes[candidate] + 1
-        else:
+        if candidate not in candidate_votes:
+            candidate_votes.get(candidate)
             candidate_votes[candidate] = 1
-
+        candidate_votes[candidate] = candidate_votes[candidate] + 1
+       
+            
 filename = ("election.txt")
 with open(filename, 'w') as txt_file:
     txt_file.write(filename)
@@ -42,6 +43,9 @@ with open(filename, 'w') as txt_file:
 
 # the winner of the election based on popular vote.
 for candidate in candidate_votes:
+
+    votes=candidate_votes.get(candidate)
+    vote_percentage=float(votes)/float(total_votes)*100
 
     if (votes > winner_votes):
         winner_votes = votes
